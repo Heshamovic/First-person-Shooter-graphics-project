0e7ed612fc974f2637ef314eb21b712f9acae9d1
Heshamovic <mohamed.97198@gmail.com>
2019-04-15 00:58:58 +0200

    Initial commit

---
+++ Graphics/GraphicsForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Graphics
+{
+    partial class GraphicsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(765, 439);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.simpleOpenGlControl1_KeyPress);
+            this.simpleOpenGlControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.simpleOpenGlControl1_MouseMove);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(794, 350);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Frame";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(850, 349);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(135, 22);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "50";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(549, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "X:";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(549, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Y:";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(549, 102);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Z:";
+            this.label8.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(822, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 39);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Stand";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(822, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 39);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Attack1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(822, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 39);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Attack2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(822, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(151, 39);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Run";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(822, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(151, 39);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Spell1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(822, 237);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(151, 39);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Spell2";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(822, 282);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(151, 39);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Death";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(794, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "speed";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(850, 386);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.Text = "0.01";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // GraphicsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 439);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "GraphicsForm";
+            this.Text = "Graphics";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GraphicsForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Graphics/Renderer.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Tao.OpenGl;
+using GlmNet;
+using System.IO;
+using Graphics._3D_Models;
+namespace Graphics
+{
+    class Renderer
+    {
+        Shader sh;
+        uint groundtextBufferID2;
+        uint groundtextBufferID1;//grass
+        uint groundtextBufferID3;//wall
+        uint groundtextBufferID4;//sky
+        Texture dn , upp , lf , rt , bk , ft ;
+        int modelID;
+        int viewID;
+        int projID;
+        int transID;
+
+        int EyePositionID;
+        int AmbientLightID;
+        int DataID;
+
+        mat4 ProjectionMatrix;
+        mat4 ViewMatrix;
+        mat4 down , up , left , right , front , back;
+        public float Speed = 1;
+
+        Model3D building , house;
+        Model3D car;
+        Model3D tree;
+        public md2LOL zombie;
+        public md2LOL blade;
+
+
+        public Camera cam;
+
+        public void Initialize()
+        {
+
+            string projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            sh = new Shader(projectPath + "\\Shaders\\SimpleVertexShader.vertexshader", projectPath + "\\Shaders\\SimpleFragmentShader.fragmentshader");
+            
+            dn = new Texture(projectPath + "\\Textures\\sandcastle_dn.png", 2, true);
+            lf = new Texture(projectPath + "\\Textures\\sandcastle_lf.png", 2, true);
+            rt = new Texture(projectPath + "\\Textures\\sandcastle_rt.png", 2, true);
+            upp = new Texture(projectPath + "\\Textures\\sandcastle_up.png", 2, true);
+            ft = new Texture(projectPath + "\\Textures\\sandcastle_ft.png", 2, true);
+            bk = new Texture(projectPath + "\\Textures\\sandcastle_bk.png", 2, true);
+
+          /*  float[] ground = {
+                -1,0,1,    1,0,0,
+                1,0,1,     1,0,0,
+                -1,0,-1,   1,0,0,
+                1,0,1,     1,0,0,
+                -1,0,-1,   1,0,0,
+                1,0,-1,    1,0,0
+            };*/
+            float groundX = 1, groundY = 0, groundZ = 1;
+
+            float[] ground = {
+                -groundX, -groundY, groundZ,    1,0,0,
+                 groundX, -groundY, -groundZ,   1,0,0,
+                -groundX, -groundY, -groundZ,    1,0,0,
+                                               
+                 groundX, -groundY, groundZ, 1,0,0,
+                -groundX, -groundY, groundZ, 1,0,0,
+                 groundX, -groundY, -groundZ, 1,0,0
+            };
+            int gsize = 1;
+            float[] groundTex = {
+                0,gsize,
+                gsize,0,
+                0,0,
+                gsize,gsize,
+                0,gsize,
+                gsize,0,
+            };
+            int wallsize = 1;
+            float[] wallTex = {
+                0,wallsize,
+                wallsize,0,
+                0,0,
+                wallsize,wallsize,
+                0,wallsize,
+                wallsize,0,
+            };
+            float skysize = 1;
+            float[] skyTex = {
+                0,skysize,
+                skysize,0,
+                0,0,
+                skysize,skysize,
+                0,skysize,
+                skysize,0,
+            };
+            groundtextBufferID2 = GPU.GenerateBuffer(ground);
+            groundtextBufferID1 = GPU.GenerateBuffer(groundTex);
+            groundtextBufferID3 = GPU.GenerateBuffer(wallTex);
+            groundtextBufferID4 = GPU.GenerateBuffer(skyTex);
+
+            // Add a buidling model, file name is "Building 02.obj"
+            // Steps: 
+            // 1- make a new instance of Model3D class, and name the variable as building
+            // 2- load the building object "Building 02.obj"
+            // 3- Add scaling and translation accordingly (for the building most probably you will not need a rotation)
+            int sz = 9000;
+            down = MathHelper.MultiplyMatrices(new List<mat4>()
+            {    
+                glm.scale(new mat4(1), new vec3(sz, sz, sz)),
+                glm.translate(new mat4(1),new vec3(0,-400,0))
+                
+            });
+            up = MathHelper.MultiplyMatrices(new List<mat4>()
+            {    
+                 glm.scale(new mat4(1), new vec3(sz, sz, sz)),
+                glm.translate(new mat4(1),new vec3(0,sz + sz - 450 ,0))
+                
+            });
+            left = MathHelper.MultiplyMatrices(new List<mat4>()
+            {    
+                glm.scale(new mat4(1), new vec3(sz, sz, sz)),
+                glm.rotate(90.0f / 180.0f * 3.1412f, new vec3(0, 0, 1)) ,
+                glm.translate(new mat4(1),new vec3(-sz,sz - 400,0)),
+            });
+            right = MathHelper.MultiplyMatrices(new List<mat4>()
+            {    
+                glm.scale(new mat4(1), new vec3(sz, sz, sz)),
+                glm.rotate(90.0f / 180.0f * 3.1412f, new vec3(0, 0, 1)) ,
+                glm.translate(new mat4(1),new vec3(sz,sz - 400,0))
+                
+            });
+            front = MathHelper.MultiplyMatrices(new List<mat4>()
+            {    
+                glm.scale(new mat4(1), new vec3(sz, sz, sz)),
+                glm.rotate(90.0f / 180.0f * 3.1412f, new vec3(1, 0, 0)) ,
+                glm.translate(new mat4(1),new vec3(0,sz - 400,-sz))
+                
+            });
+            back = MathHelper.MultiplyMatrices(new List<mat4>()
+            {    
+                glm.scale(new mat4(1), new vec3(sz, sz, sz)),
+                glm.rotate(90.0f / 180.0f * 3.1412f, new vec3(1, 0, 0)) ,
+                glm.translate(new mat4(1),new vec3(0,sz - 400,sz))
+                
+            });
+
+            building = new Model3D();
+            building.LoadFile(projectPath + "\\ModelFiles\\static\\building", "Building 02.obj", 1);
+            building.scalematrix = glm.scale(new mat4(1), new vec3(300, 300, 300));
+            building.transmatrix = glm.translate(new mat4(1), new vec3(1, 1, 1));
+
+
+            house = new Model3D();
+            house.LoadFile(projectPath + "\\ModelFiles\\static\\House", "house.obj", 1);
+            house.scalematrix = glm.scale(new mat4(1), new vec3(300, 300, 300));
+            house.transmatrix = glm.translate(new mat4(1), new vec3(4000, -400, 4000));
+            ////////////////////
+            //// Similarly Add a tree inside the building, file name is "Tree.obj"
+
+
+            tree = new Model3D();
+            tree.LoadFile(projectPath + "\\ModelFiles\\static\\tree", "tree.obj", 4);
+            tree.scalematrix = glm.scale(new mat4(1), new vec3(100, 100, 100));
+            tree.transmatrix = glm.translate(new mat4(1), new vec3(1, -400, 4000));
+
+
+            ////////////////////
+            //// Similarly Add a car outside the building, file name is "dvp.obj"
+
+            car = new Model3D();
+            car.LoadFile(projectPath + "\\ModelFiles\\static\\car", "dpv.obj", 3);
+            car.scalematrix = glm.scale(new mat4(1), new vec3(1, 1, 1));
+            car.transmatrix = glm.translate(new mat4(1), new vec3(-500, 1, -100));
+            car.rotmatrix = glm.rotate(3.1412f, new vec3(0, 1, 0));
+
+            ////////////////////
+            ////////////////////
+            //// Add an animated model, file name is "zombie.md2" with animation: "Stand"
+            /// Steps:
+            // 1- make a new instance of MD2LOL or MD2 class, while instanciating the new model, it loads the model
+            // 2- start animating the object using StartAnimation method which is inside the md2/md2LOL 
+            // 3- Add rotation, scaling and translation accordingly 
+
+            zombie = new md2LOL(projectPath + "\\ModelFiles\\animated\\md2LOL\\fiora.md2");
+            zombie.StartAnimation(animType_LOL.STAND);
+            zombie.rotationMatrix = glm.rotate((float)((-90.0f / 180) * Math.PI), new vec3(1, 0, 0));
+            zombie.scaleMatrix = glm.scale(new mat4(1), new vec3(10, 10, 10));
+            zombie.TranslationMatrix = glm.translate(new mat4(1), new vec3(1, 1, 1));
+
+            ////////////////////
+            /// Similarly, Add an animated model, file name is "Blade.md2" with animation: "Run"
+
+            blade = new md2LOL(projectPath + "\\ModelFiles\\animated\\md2LOL\\garen.md2");
+            blade.StartAnimation(animType_LOL.STAND);
+            blade.rotationMatrix = glm.rotate((float)((-90.0f / 180) * Math.PI), new vec3(1, 0, 0));
+            blade.scaleMatrix = glm.scale(new mat4(1), new vec3(5, 5, 5));
+            blade.TranslationMatrix = glm.translate(new mat4(1), new vec3(1, 1, 1));
+
+            //////////////////////////////////////////////////////////////////////////////
+
+            Gl.glClearColor(0, 0, 0.4f, 1);
+
+             
+
+
+
+            
+            cam = new Camera();
+            cam.Reset(0, 34, 55, 0, 0, 0, 0, 1, 0);
+
+            ProjectionMatrix = cam.GetProjectionMatrix();
+            ViewMatrix = cam.GetViewMatrix();
+
+            transID = Gl.glGetUniformLocation(sh.ID, "model");
+            projID = Gl.glGetUniformLocation(sh.ID, "projection");
+            viewID = Gl.glGetUniformLocation(sh.ID, "view");
+
+            sh.UseShader();
+
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glDepthFunc(Gl.GL_LESS);
+        }
+
+        public void Draw()
+        {
+
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT|Gl.GL_DEPTH_BUFFER_BIT);
+            sh.UseShader();
+           
+            Gl.glUniformMatrix4fv(transID, 1, Gl.GL_FALSE, down.to_array());
+            Gl.glUniformMatrix4fv(projID, 1, Gl.GL_FALSE, ProjectionMatrix.to_array());
+            Gl.glUniformMatrix4fv(viewID, 1, Gl.GL_FALSE, ViewMatrix.to_array());
+
+            Gl.glUniform3fv(EyePositionID, 1, cam.GetCameraPosition().to_array());
+
+           
+
+            GPU.BindBuffer(groundtextBufferID2);
+            Gl.glEnableVertexAttribArray(0);
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), IntPtr.Zero);
+            Gl.glEnableVertexAttribArray(1);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)(3 * sizeof(float)));
+
+            GPU.BindBuffer(groundtextBufferID1);
+            Gl.glEnableVertexAttribArray(2);
+            Gl.glVertexAttribPointer(2, 2, Gl.GL_FLOAT, Gl.GL_FALSE, 0, IntPtr.Zero);
+
+            dn.Bind();
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+
+
+            GPU.BindBuffer(groundtextBufferID4);
+            Gl.glEnableVertexAttribArray(2);
+            Gl.glVertexAttribPointer(2, 2, Gl.GL_FLOAT, Gl.GL_FALSE, 0, IntPtr.Zero);
+
+                         
+            upp.Bind();
+            Gl.glUniformMatrix4fv(transID, 1, Gl.GL_FALSE, up.to_array());
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+
+            GPU.BindBuffer(groundtextBufferID3);
+            Gl.glEnableVertexAttribArray(2);
+            Gl.glVertexAttribPointer(2, 2, Gl.GL_FLOAT, Gl.GL_FALSE, 0, IntPtr.Zero);
+
+            lf.Bind();
+            Gl.glUniformMatrix4fv(transID, 1, Gl.GL_FALSE, left.to_array());
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+
+            rt.Bind();
+            Gl.glUniformMatrix4fv(transID, 1, Gl.GL_FALSE, right.to_array());
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+
+            ft.Bind();
+            Gl.glUniformMatrix4fv(transID, 1, Gl.GL_FALSE, front.to_array());
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+
+            bk.Bind();
+            Gl.glUniformMatrix4fv(transID, 1, Gl.GL_FALSE, back.to_array());
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+
+            
+            // For each of your model, 
+            // 1- call the draw method
+            // 2- Pass to it the ID of the model in the vertex shader so that it can apply the correct transformations on its vertices
+          //  zombie.Draw(modelID);
+            Gl.glDepthFunc(Gl.GL_LEQUAL);
+
+            building.Draw(transID);
+            house.Draw(transID);
+            Gl.glDepthFunc(Gl.GL_LESS);
+
+            tree.Draw(transID);
+            blade.Draw(transID);
+            car.Draw(transID);
+        }
+        public void Update(float deltaTime)
+        {
+            cam.UpdateViewMatrix();
+            ProjectionMatrix = cam.GetProjectionMatrix();
+            ViewMatrix = cam.GetViewMatrix();
+
+            // For the animated models, call the UpdateAnimation of the model so that it can interpolate the vertices to the correct position
+
+            zombie.UpdateAnimation();
+            blade.UpdateAnimation();
+        }
+        public void CleanUp()
+        {
+            sh.DestroyShader();
+        }
+    }
+}
